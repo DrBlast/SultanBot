b4a8de73297dad44163bcc1d5c0ecf7cda48e026
Evgeniy Nichegovskiy <eugene.nichegovskiy@gmail.com>
2020-12-04 03:37:58 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # SultanBot
+
+[Инструкция на русском]()
+
+## Installation
+1. Install last version of [AnkuLua](https://ankulua.boards.net/thread/1395/ankulua-trial-apk-download)
+2. Grant permissions for AnkuLua
+3. Download [archive]() with bot
+4. Unzip archive to Ankulua folder on your phone
+5. Run AnluLua program
+6. Tap Select script
+7. Open sultan bot folrder and select file runBot.lua
+8. Tap Start service
+9. Start game
+
+## Flags:
+**Experience** - collect experience in Palace
+
+**Puzzles** - collect gun, wheel, go to guards regiment and make 6 walks and 6 visits in harem for twin accounts. May not work on new puzzles icon
+
+**Trip** - send heirs to trip in ministery
+
+**Friends** - send vizirs to your friends in academy. Be sure that you have limit for 11 vizirs
+
+**Stars** - play Star Stone Blessing minigame. You should start new game.
+
+**Regiment** - play guards regiment. You should start new battle
+
+**Arena - continue fight**  - start new arena battle and set number of bonuses applied every move. Then start bot.
+
+**Immersive Mode** - if bit not collect resouses try set it to opposite value
+
+
+When script ends, bot collects daily bonuses
+
+
+## Donate
+If this project helped you, you can give me a cup of coffee 
---
+++ sultan/sultanObjects.lua
@@ -0,0 +1,167 @@
+--main
+alliance = Pattern("alliance.png")
+xx = Pattern("x.png")
+xx2 = Pattern("xx.png")
+---
+bonusImagePath = "/bonus"
+chertog = Pattern("chertog.png")
+bonus = Pattern("bonus.png")
+bonus2 = Pattern("bonus2.png")
+serverRate = Pattern("server-rating.png")
+allServerRate = Pattern("all-server-rating.png")
+campanyBonus = Pattern("company.png")
+alliancesBonus = Pattern("alliances.png")
+intimacyBonus = Pattern("intimacy.png")
+repBonus = Pattern("reputation")
+---
+autoCollect = Pattern("avtosbor.png")
+back = Pattern("back.png")
+back = Pattern("back2.png")
+coffee = Pattern("coffee.png")
+collectRes = Pattern("collect.png")
+dvorec = Pattern("dvorec.png")
+--
+gadalka = Pattern("gadalka.png")
+littleChest = Pattern("lchest2.png")
+chestPoint = Pattern("chestpoint.png")
+--
+melek = Pattern("melek.png")
+notCollected = Pattern("notcollected.png")
+
+orders = Pattern("order.png")
+sinan = Pattern("sinan.png")
+
+---
+tasks = Pattern("tasks.png")
+achievements = Pattern("achievements.png")
+gift = Pattern("gift.png")
+bgift = Pattern("bgift.png")
+shop = Pattern("shop.png")
+---
+ministeryImagePath = "/ministery"
+ministery = Pattern("minitery.png")
+m1 = Pattern("m1.png")
+m2 = Pattern("m2.png")
+m3 = Pattern("m3.png")
+m4 = Pattern("m4.png")
+box = Pattern("box.png")
+ok = Pattern("ok.png")
+chance = Pattern("chance.png")
+cbxSendAll = Pattern("sendAll.png")
+cbxSendAllSelected = Pattern("sendAll-selected.png")
+sendBtn = Pattern("sendBtn.png")
+startTrip = Pattern("sendToLearn.png")
+addMore = Pattern("add.png")
+confirmSend = Pattern("done.png")
+scroll = Pattern("scroll.png")
+---
+academyImg = Pattern("academy.png")
+addVizir = Pattern("add.png")
+avtoAddVizirs = Pattern("autoadd.png")
+autoComplete = Pattern("autocomplete.png")
+oneComplete = Pattern("complete.png")
+sendVizir = Pattern("send.png")
+takeAway = Pattern("takeaway.png")
+friends = Pattern("friends.png")
+selectFriend = Pattern("selectfriend.png")
+selectFriend2 = Pattern("selectfriend2.png")
+vizirAcademyFrame = Pattern("viz-frame.png")
+addPractice = Pattern("addpractice.png")
+countPractice =  Pattern("countPractice.png")
+
+---
+haremPath = "/harem"
+harem = Pattern("harem.png")
+consort = Pattern("consort.png")
+heir = Pattern("heir.png")
+inroom = Pattern("inroom.png")
+renew = Pattern("renew.png")
+random = Pattern("random.png")
+autoVisit = Pattern("auto.png")
+closeBtn = Pattern("close.png")
+learn = Pattern("learn.png")
+learnAll = Pattern("learnAll.png")
+allLearned=Pattern("allLearned.png")
+renewLearn = Pattern("renewLearn.png")
+addHeir = Pattern("addHeir.png")
+newMoves = Pattern("newmoves.png")
+
+---
+townPath = "/town"
+exitToTown = Pattern("exittotown.png")
+renewWalk = Pattern("renew-walk.png")
+walk = Pattern("walk.png")
+autoWalk = Pattern("auto.png")
+energy = Pattern("energy.png")
+----
+arenaPath = "/arena"
+arena = Pattern("arena.png")
+fight = Pattern("fight.png")
+startFight = Pattern("start.png")
+startFightScreen = Pattern("start-layout.png")
+fightScreen = Pattern("fight-screen.png")
+vs = Pattern("vs.png")
+arenaBox = Pattern("arena-box.png")
+points1 = Pattern("1points.png")
+points2 = Pattern("2points.png")
+approveNoPoints = Pattern("approveNoPoints.png")
+vizirFrame = Pattern("vizirFrame.png")
+
+autoFight = Pattern("avtoboy.png")
+withoutBonus = Pattern("notnow.png")
+arenaBonus = Pattern("arenabonus.png")
+
+approve = Pattern("approve.png")
+boxText = Pattern("box-text.png")
+boxText2 = Pattern("box-text2.png")
+nextVizir = Pattern("next.png")
+
+turnedOff = Pattern("turnedoff.png")
+turnOff = Pattern("turnOffBonus.png")
+
+closeBonus = Pattern("xx.png")
+
+puzzleIcon = Pattern("puzzles.png")
+puzzleIcon2 = Pattern("puzzles2.png")
+puzzleIcon3 = Pattern("puzzles3.png")
+vizirs = Pattern("vizirs.png")
+puzzlePath = "/puzzles"
+gun =  Pattern("gun.png")
+shoot = Pattern("shoot.png")
+shootDescr = Pattern("shoot-descr.png")
+shootDescr2 = Pattern("shoot-descr2.png")
+wheel = Pattern("wheel.png")
+wheelBtn = Pattern("wheel-btn.png")
+wheelFinished = Pattern("wheel-finished.png")
+
+lvl1 = Pattern("lvl1.png")
+lvl2 = Pattern("lvl2.png")
+lvl3 = Pattern("lvl3.png")
+lvl4 = Pattern("lvl4.png")
+lvl5 = Pattern("lvl5.png")
+lvl6 = Pattern("lvl6.png")
+lvl7 = Pattern("lvl7.png")
+lvl8 = Pattern("lvl8.png")
+
+rgmtPath = "/rgmt"
+nextFight = Pattern("continue.png")
+skipAnim = Pattern("skip.png")
+newFight = Pattern("fight.png")
+polk = Pattern("polk.png")
+training = Pattern("trainning.png")
+chooseFightingLvl = Pattern("chooselvl.png")
+
+stars1 = Pattern("stars.png")
+stars2 = Pattern("stars2.png")
+stars3 = Pattern("stars3.png")
+oneMore = Pattern("onemore.png")
+
+line1 = Pattern("line1.png")
+line2 = Pattern("line2.png")
+line3 = Pattern("line3.png")
+
+--
+kitchenPath = "/kitchen"
+trap = Pattern("trap.png")
+commonGame = Pattern("common.png")
+
